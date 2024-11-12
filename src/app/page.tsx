92b535cfffb8f9b50f8592804eec6ef0f14f6491
Rida <samiaazam50@gmail.com>
2024-11-13 01:17:42 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
-import Image from "next/image";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
 import Hero from "./components/hero";
 import RecentPost from "./components/recentPost";
 import FeaturedWork from "./components/featuredWork";
 
 export default function Home() {
   return (
-    <div >
-
-
-
-
- <Hero/>
- <RecentPost/>
-   {/* Featured Work Section */}
-  <FeaturedWork />
-      
+    <div>
+      <Hero />
+      <RecentPost />
+      {/* Featured Work Section */}
+      <FeaturedWork />
     </div>
   );
 }
